c058e21ac103068122ff38d58c80b5dc09bb1531
ipb23.s.mitniece <ipb23.s.mitniece@pjvt.local>
2024-11-28 09:46:56 +0200

    izveidoju php failu, lai mācītos

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+echo    "<style>
+            body {
+                background-color: #FC8EAC;
+                color: white;
+                font-size: 20px;
+            }
+        </style>";
+
+echo "yippee <br><br>";
+
+// izvadi visus pārā skaitļus no 1-100
+for ($x = 2; $x <= 100; $x+=2) {
+    echo "$x <br>";
+}
+
+
+
+
+
+// while cikls
+// $y = 1
+// while ($y <= 100) {
+//      if ($y % 2 == 0){
+//          echo "$x <br>";
+//      }
+//      $y = $y + 1;
+// }
+
+
+// ko dara %
+// izvada atlikumu
+// % - dala, peim., 13 % 5 = 3 (atlikums)
+// 4 % 5 = 1 (nulli pārvērš pa 1)
+?>
