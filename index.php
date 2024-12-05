c27f90425df6f2a80633aa8424a6ff5c273b4888
ipb23.s.mitniece <ipb23.s.mitniece@pjvt.local>
2024-12-05 14:47:31 +0200

    faktoriāli

--- index.php
+++ index.php
@@ -11,13 +11,10 @@ echo    "<style>
 echo "yippee <br><br>";
 
 // izvadi visus pārā skaitļus no 1-100
-for ($x = 2; $x <= 100; $x+=2) {
-    echo "$x <br>";
+echo "<b>1. uzdevums</b><br>";
+for ($a = 2; $a <= 100; $a+=2) {
+    echo "$a <br>";
 }
-
-
-
-
 
 // while cikls
 // $y = 1
@@ -28,9 +25,26 @@ for ($x = 2; $x <= 100; $x+=2) {
 //      $y = $y + 1;
 // }
 
-
 // ko dara %
 // izvada atlikumu
 // % - dala, peim., 13 % 5 = 3 (atlikums)
 // 4 % 5 = 1 (nulli pārvērš pa 1)
+
+
+//faktoriāls 10
+//!3 = 3*2*1
+echo "<br><b>2. uzdevums</b><br>";
+$c = 1;
+for ($b = 1; $b <= 10; $b+=1) {
+    $c = $c*$b;
+}
+echo "$c <br><br>";
+
+//saskaita visus naturālos skaitļus no 1 - 22
+$e = 0;
+for ($d = 1; $d <= 22; $d++) {
+    $e = $e + $d;
+}
+echo "$e";
+
 ?>
